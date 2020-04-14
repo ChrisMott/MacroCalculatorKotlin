2e936593460ad827fe75f82e8eb19fbfd04cfa44
Chris Mott <9664841+ChrisMott@users.noreply.github.com>
2020-04-14 01:29:00 -0500

    adds arrayList functionality and iterates through

--- MacroCalculator.kt
+++ MacroCalculator.kt
@@ -1,16 +1,34 @@
 
+
+import java.util.*
+import kotlin.collections.ArrayList
+
 //data class Food(var name:String, var servings:Double, var calories:Double, var carbs:Double, var fat:Double, var protein:Double)
 
 fun main(){
+
+    //var allFoodMacros = arrayOf<Food>()
+
+    val allFoodMacros: MutableList<Food> = ArrayList()
+
 
     var greenBeans = Food("Green Beans", 4.0, 140.0, 24.0, 0.0, 8.0 )
 
     var eggs = Food("Eggs", 1.0, 20.0, 5.0, 12.0, 16.0)
 
-    println(greenBeans.toString())
+    allFoodMacros.add(greenBeans)
+    
 
-    println(eggs.toString())
+    allFoodMacros.add(eggs)
 
+
+    for(food in allFoodMacros){
+
+        println(food)
+
+
+
+    }
 
 
 }
